3176388528432f796c6b5da3ecefac903dbc49bd
Quinn Kennedy <qck@o804-5.eecs.cwru.edu>
2018-04-30 12:01:56 -0400

    Initial Commit

---
+++ SpotKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotKeeper
+//
+//  Created by Quinn Kennedy on 4/30/18.
+//  Copyright © 2018 Quinn Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
